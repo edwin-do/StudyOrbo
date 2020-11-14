908422d4337ecd11d738d19521524bd1c19f877b
edwin-do <doedwin123@gmail.com>
2020-11-14 17:10:28 -0500

    fix sessionStorage

--- js/join.js
+++ js/join.js
@@ -3,10 +3,16 @@ function joinGroup(){
     sessionStorage.groupID = document.getElementById("groupID").value;
 
     if (sessionStorage.groupID == "231456"){
-        location.href="../pages/join_success.html"
+        location.href="../pages/join_intro.html"
     }
     else{
         document.getElementById("message").textContent = "The groupID " + sessionStorage.groupID + " does not exist";
         document.getElementById("groupID").textContent = "";
     }
 };
+
+function joinIntro(){
+    
+    sessionStorage.joinName = document.getElementById('joinName').value;
+    sessionStorage.joinEmail = document.getElementById('joinEmail').value;
+}
