f7b06603416a6951d7ad62542a0dc8bb9737e062
edwin-do <doedwin123@gmail.com>
2020-11-15 13:15:18 -0500

    fix bug on browse for login

--- js/join.js
+++ js/join.js
@@ -2,7 +2,13 @@ function joinGroup(){
     sessionStorage.groupID = document.getElementById("groupID").value;
 
     if (sessionStorage.groupID == "231456"){
-        location.href="../pages/join_intro.html"
+        if(sessionStorage.joinName){
+            location.href="../pages/join_success.html"
+        }
+        else{
+            location.href="../pages/join_intro.html"
+        }
+
     }
     else{
         document.getElementById("message").textContent = "The groupID " + sessionStorage.groupID + " does not exist";
